7831f66a0019c1b91c854b19450bce41e87bc56d
Daniel Barradas <daniel.barradas@gmail.com>
2025-04-14 15:36:11 +0100

    Update __init__.py

--- i2s_microfone/__init__.py
+++ i2s_microfone/__init__.py
@@ -2,5 +2,6 @@ from esphome.components import sensor
 import esphome.codegen as cg
 import esphome.config_validation as cv
 
+CODEOWNERS = ["@DanBar1971"]
 DEPENDENCIES = ["logger"]
 AUTO_LOAD = ["sensor"]
